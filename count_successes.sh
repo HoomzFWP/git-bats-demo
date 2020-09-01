968f1296b03bfbd3181e4820a2973debeaa44b0c
hoomzfwp <ndamte@gmail.com>
2020-08-31 21:26:24 -0500

    Got the `count_successes.sh` script working.
    It now passes all the tests and appears to be in good shape, ready for a code review.

--- count_successes.sh
+++ count_successes.sh
@@ -1,3 +1,24 @@
 #!/usr/bin/bash
 
-echo "There were 78 successes and 22 failures."
+# Extracts contents of a compressed tar archive specified as
+# the first command line argument and counts the number of files
+# that contain the string "SUCCESS" and the string "FAILURE"
+# and reports the result
+
+# Hoomz Damte
+# University of Minnesota, Morris
+# August 2020
+tar_file=$1
+
+scratch=`mktemp --directory`
+
+tar zxf $tar_file --directory $scratch
+
+numSuccesses=`grep -lr SUCCESS $scratch | wc -l`
+numFailures=`grep -lr FAILURE $scratch | wc -l`
+
+tar zxf $tar_file --directory $scratch
+
+echo "There were $numSuccesses successes and $numFailures failures." 
+
+rm -rf $scratch
